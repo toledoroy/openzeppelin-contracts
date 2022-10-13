43a712f4922b0123810939237bc2954db33f14de
Roy <toledoroy@gmail.com>
2022-10-13 14:25:46 -0700

    Comments & organization changes

--- contracts/token/ERC1155/extensions/ERC1155VotesNF.sol
+++ contracts/token/ERC1155/extensions/ERC1155VotesNF.sol
@@ -6,15 +6,16 @@ import "../ERC1155.sol";
 import "../../../governance/utils/Votes.sol";
 
 /**
- * @dev Extension of ERC1155 to support voting and delegation as implemented by {Votes}, where each individual NFT counts
- * as 1 vote unit.
+ * @dev Extension of ERC1155 to support voting and delegation as implemented by {Votes}.
+ * Treat as non-fungible -- voting power is equal to the total amuount of tokens held multiplied by the token's power
+ * (as determind by {powerOfToken}).
  *
  * Tokens do not count as votes until they are delegated, because votes must be tracked which incurs an additional cost
  * on every transfer. Token holders can either delegate to a trusted representative who will decide how to make use of
  * the votes in governance decisions, or they can delegate to themselves to be their own representative.
  *
  */
-abstract contract ERC1155Votes is ERC1155, Votes {
+abstract contract ERC1155VotesNF is ERC1155, Votes {
     // Track the current undelegated balance for each account.
     // this allows to support different voting power for different tokens
     mapping(address => uint256) private _unitsBalance;
@@ -23,8 +24,15 @@ abstract contract ERC1155Votes is ERC1155, Votes {
      * @dev Calculate the voting power of each token
      * token weight expected to remain consistent and immutable.
      */
-    function powerOfToken(uint256) public pure virtual returns (uint256) {
+    function powerOfToken(uint256) public view virtual returns (uint256) {
         return 1;
+    }
+
+    /**
+     * @dev Must return the voting units held by an account.
+     */
+    function _getVotingUnits(address account) internal view override returns (uint256) {
+        return _unitsBalance[account];
     }
 
     /**
@@ -39,15 +47,14 @@ abstract contract ERC1155Votes is ERC1155, Votes {
         bytes memory data
     ) internal virtual override {
         for (uint256 i = 0; i < ids.length; ++i) {
-            uint256 id = ids[i];
-            uint256 amount = amounts[i];
-            _transferVotingUnits(from, to, powerOfToken(id) * amount);
+            uint256 units = (powerOfToken(ids[i]) * amounts[i]);
+            _transferVotingUnits(from, to, units);
         }
         super._afterTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
     /**
-     * @dev Track all power-adjusted balanced
+     * @dev Track all power-adjusted balances
      */
     function _transferVotingUnits(
         address from,
@@ -65,10 +72,4 @@ abstract contract ERC1155Votes is ERC1155, Votes {
         super._transferVotingUnits(from, to, amount);
     }
 
-    /**
-     * @dev Must return the voting units held by an account.
-     */
-    function _getVotingUnits(address account) internal view override returns (uint256) {
-        return _unitsBalance[account];
-    }
 }
